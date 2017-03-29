f2dc42e00368c75e206c91de7cc70b85078829b4
Jin Hong Jeon <hong9820@Jeon-ui-MacBook-Pro.local>
2017-03-29 20:09:36 +0900

    Initial commit :)

---
+++ FastcampusFrameJimmy/FastcampusFrameJimmy.h
@@ -0,0 +1,19 @@
+//
+//  FastcampusFrameJimmy.h
+//  FastcampusFrameJimmy
+//
+//  Created by Jin Hong Jeon on 2017. 3. 29..
+//  Copyright © 2017년 Jimmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FastcampusFrameJimmy.
+FOUNDATION_EXPORT double FastcampusFrameJimmyVersionNumber;
+
+//! Project version string for FastcampusFrameJimmy.
+FOUNDATION_EXPORT const unsigned char FastcampusFrameJimmyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FastcampusFrameJimmy/PublicHeader.h>
+
+
